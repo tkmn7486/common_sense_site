9789715d76c98a1454b48c56b2731b210224db94
tkmn7486 <71046924+tkmn7486@users.noreply.github.com>
2022-02-23 21:45:00 +0900

    add_repository

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,101 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <div class="app">
+    <div class="title">
+      <!-- <img class="logo" alt="logo" src="./assets/logo.png"> -->
+      <h2 class="logo">コモセン.WEB</h2>
+    </div>
+    <div id="nav">
+      <router-link to="/">おすすめ</router-link>
+      <router-link to="/about">ジャンル別</router-link>
+      <router-link to="/about">このサイトについて</router-link>
+      <router-link to="/about">ご提案・お問合せ</router-link>
+    </div>
+    <router-view/>
   </div>
-  <router-view/>
 </template>
 
 <style>
+*{
+  margin: 0px;
+  padding: 0px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: rgba(38,56,89,1);
 }
 
 #nav {
-  padding: 30px;
+  padding: 15px;
+  text-align:center;
+  background-color:rgba(23,34,59,1);
+  border:double 5px #6b778d;
+  border-right:none;
+  border-left:none;
+  border-bottom:none;
 }
 
 #nav a {
+  margin:0 10px 0 10px;
   font-weight: bold;
-  color: #2c3e50;
+  color: #6b778d;
+}
+
+router-link{
+  margin-left:20px;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: rgba(255,103,104,1);
+}
+
+.title{
+  background-color:rgba(23,34,59,1);
+  padding:10px;
+}
+
+.logo{
+  padding-left:10px;
+  color:white;
+}
+
+/* スマホ用CSS */
+@media screen and (max-width:480px) {
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+#nav {
+  padding: 10px;
+  text-align:center;
+  border-right:none;
+  border-left:none;
+  border-bottom:none;
+}
+
+#nav a {
+  font-size:11px;
+  margin:0 10px 0 10px;
+  font-weight: bold;
+}
+
+router-link{
+  margin-left:20px;
+}
+
+.title{
+  padding:10px;
+}
+
+.logo{
+  padding-left:10px;
+}
+
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,143 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="recommend_cont">
+      <h3 class="recommend_title">>>おすすめ</h3>
+      <div class="list_contents">
+        <div class="content_border" v-for="reco_cont in reco_list" :key="reco_cont.id" @click="open_artic">
+          <div class="content">
+            <h4 class="content_title">{{reco_cont.title}}</h4>
+            <div class="tags">
+              <div class="tag_list" v-for="tags in reco_cont.tag" :key="tags.id">
+                <button class="tag_button" @click="search_from_tag">{{tags}}</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import {ref} from 'vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  setup(){
+    let reco_list = ref([
+      {id:0, title:"お焼香の作法", comment:"仏式葬儀のお焼香作法について", tag:["葬儀","作法","仏教"]},
+      {id:1, title:"確定申告とは", comment:"確定申告って何？という方のための要約", tag:["税金","年末","副業"]},
+      {id:1, title:"確定申告とは", comment:"確定申告って何？という方のための要約", tag:["税金","年末","副業"]},
+      {id:1, title:"確定申告とは", comment:"確定申告って何？という方のための要約", tag:["税金","年末","副業"]},
+      {id:1, title:"確定申告とは", comment:"確定申告って何？という方のための要約", tag:["税金","年末","副業"]},
+    ])
+    let cont_list = ref([])
+    return{
+      reco_list,
+      cont_list,
+    }
   }
+
 }
 </script>
+
+<style>
+  .recommend_title{
+    margin:10px;
+    border-bottom:double 5px;
+    width:130px;
+  }
+
+  .content_border{
+    border:solid 1px;
+    border-radius:10px;
+    margin:10px;
+    width:30%;
+    display:inline-block;
+    box-shadow: 0 10px 15px 0 rgba(0, 0, 0, .5);
+  }
+
+  .content{
+    text-align:center;
+    padding:20px;
+  }
+
+  .content_title{
+    margin:5px 0 0 5px;
+  }
+
+  .content_comment{
+    margin:5px 0 0 5px;
+  }
+
+  .tags{
+    margin-top:10px;
+  }
+
+  .tag_button{
+    border:solid 1px;
+    border-radius:5px;
+    background-color:white;
+    font-size:5px;
+    text-align:center;
+    padding:0 3px 0 3px;
+    margin:0 3px 0 3px;
+  }
+
+  .tag_list{
+    display:inline-block;
+  }
+
+/* スマホ用CSS */
+@media screen and (max-width:480px) {
+
+  .recommend_title{
+    margin:10px;
+    border-bottom:double 5px;
+    width:130px;
+  }
+
+  .content_border{
+    border:solid 1px;
+    border-radius:10px;
+    width:40%;
+    display:inline-block;
+    box-shadow: 0 10px 15px 0 rgba(0, 0, 0, .5);
+  }
+
+  .content{
+    text-align:center;
+    padding:20px;
+  }
+
+  .content_title{
+    font-size:15px;
+    margin:5px 0 0 5px;
+  }
+
+  .content_comment{
+    margin:5px 0 0 5px;
+  }
+
+  .tags{
+    margin-top:10px;
+  }
+
+  .tag_button{
+    border:solid 1px black;
+    border-radius:5px;
+    background-color:#ffffff;
+    font-size:80%;
+    text-align:center;
+    padding:0 3px 0 3px;
+    margin:0 3px 0 3px;
+  }
+
+  .tag_list{
+    display:inline-block;
+  }
+
+}
+
+</style>
